abacee72079ac96a9651bf779198d655b31c2e16
Apil Khadka <apilkhadka2020@gmail.com>
2026-03-13 20:09:04 +0545

    style(ui): Refine logo size, Tally widget, and Coming Soon card

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import { siteConfig } from "@/data/siteConfig";
 import Image from "next/image";
 
-export const Logo = ({ theme = "white" }: { theme?: "black" | "white" }) => {
+export const Logo = ({ theme = "white", className = "" }: { theme?: "black" | "white", className?: string }) => {
   const src = theme === "white" ? siteConfig.logos.white : siteConfig.logos.black;
   
   return (
-    <div className="relative w-64 h-16 md:w-80 md:h-20">
+    <div className={`relative w-80 h-24 md:w-[450px] md:h-32 ${className}`}>
       <Image
         src={src}
         alt={siteConfig.name}
